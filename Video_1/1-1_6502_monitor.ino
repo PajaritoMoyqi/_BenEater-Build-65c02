027723e3cdc0b9a298283e3fd5dd292503184919
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-03-27 14:07:33 +0900

    [Video1]-1: print each signal comes from A0-A15 pins (DONE)

---
+++ Video_1/1-1_6502_monitor.ino
@@ -0,0 +1,30 @@
+/*
+  print each signal comes from A0-A15 pins of W65C02S
+  only address pins are connected to Arduino
+*/
+
+// connected pin numbers
+const char ADDR[] = { 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52 };
+
+void setup()
+{
+    // put setup code that runs once
+    for ( int n = 0; n < 16; n += 1 )
+    {
+        pinMode( ADDR[n], INPUT );
+    }
+}
+
+void loop()
+{
+    // put main code that runs repeatedly
+
+    // print all 16-bits
+    for ( int n = 0; n < 16; n += 1 )
+    {
+        int bit = digitalRead( ADDR[n] ) ? 1 : 0;
+        Serial.print( bit );
+    }
+    // seperate line for next input signal
+    Serial.println();
+}
