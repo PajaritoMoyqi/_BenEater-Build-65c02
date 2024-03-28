cb37f9c772ea23d9b079e0f87e369f88b5f8ea99
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-03-29 08:14:53 +0900

    [Video9]: Syntax check (DONE)

--- Video_9/9-1_hello_world.s
+++ Video_9/9-1_hello_world.s
@@ -1,107 +1,107 @@
-/*
-  EEPROM(AT28C256) is mapped from 0x8000 to 0xFFFF. - to program a computer
-  I/O controller(W65C22) is mapped from 0x6000 to 0x7FFF. - to make output data meaningful
-    I/O controller setting registers are mapped from 0x6000 to 0x600F.
-      part of PORTA is for flag bits, PORTB is for data bits when using LCD monitor connected.
-  RAM(HM62256B) is mapped from 0x0000 to 0x3FFF. - to get memory for stack(in W65C02S, stack has address from 0x0100 to 0x01FF)
-    Stack is mapped for 0x0100 to 0x01FF.
-*/
+;  ..........................................................................................
+;  EEPROM(AT28C256) is mapped from 0x8000 to 0xFFFF. - to program a computer
+;  I/O controller(W65C22) is mapped from 0x6000 to 0x7FFF. - to make output data meaningful
+;    I/O controller setting registers are mapped from 0x6000 to 0x600F.
+;      part of PORTA is for flag bits, PORTB is for data bits when using LCD monitor connected.
+;  RAM(HM62256B) is mapped from 0x0000 to 0x3FFF. - to get memory for stack(in W65C02S, stack has address from 0x0100 to 0x01FF)
+;    Stack is mapped for 0x0100 to 0x01FF.
+;  ..........................................................................................
 
-/*
-  This code shows letters on LCD monitor.
-  But not working because of the difference of speed between CPU clock cycle and instruction execution time of LCD monitor.
-  So we add 750 nop instructions to give enough time.
+;  ..........................................................................................
+;  This code shows letters on LCD monitor.
+;  But not working because of the difference of speed between CPU clock cycle and instruction execution time of LCD monitor.
+;  So we add 750 nop instructions to give enough time.
+;
+;  Cautions at subrutine, 'JRS', 'RTS', 'TXS'.
+;  ..........................................................................................
 
-  Cautions at subrutine, 'JRS', 'RTS', 'TXS'.
-*/
+PORTB = $6000 ; I/O signal through port B
+PORTA = $6001 ; I/O signal through port A
+DDRB = $6002 ; Data direction setting register for port B
+DDRA = $6003 ; Data direction setting register for port A
 
-PORTB = $6000 # I/O signal through port B
-PORTA = $6001 # I/O signal through port A
-DDRB = $6002 # Data direction setting register for port B
-DDRA = $6003 # Data direction setting register for port A
-
-# flags for PORTA
+; flags for PORTA
 E = %10000000
 RW = %01000000
 RS = %00100000
 
-  # origin directive: tells assembler where this codes below should be placed in memory
+  ; origin directive: tells assembler where this codes below should be placed in memory
   .org $8000
 
 init:
-  # initialize stack pointer to 0x01FF
+  ; initialize stack pointer to 0x01FF
   LDX #$FF
   TXS
 
   ; set data directions
-  # set all pins on port B to output
-  # which are the pins connected to D0-D7
+  ; set all pins on port B to output
+  ; which are the pins connected to D0-D7
   LDA #%11111111
   STA DDRB
-  # set only top three pins on port A to output
-  # which are the pins connected to E, R/WB, RS
+  ; set only top three pins on port A to output
+  ; which are the pins connected to E, R/WB, RS
   LDA #%11100000
   STA DDRA
 
   ; set initial setting for display of LCD monitor
-  LDA #%00111000 # 8-bit mode, 2-line display, 5x8 font
+  LDA #%00111000 ; 8-bit mode, 2-line display, 5x8 font
   JSR lcd_send_instruction
-  LDA #%00001110 # display on, cursor on, blink off
+  LDA #%00001110 ; display on, cursor on, blink off
   JSR lcd_send_instruction
-  LDA #%00000110 # increment and shift cursor, do not shift display
+  LDA #%00000110 ; increment and shift cursor, do not shift display
   JSR lcd_send_instruction
-  LDA #%00000001 # Clear display
+  LDA #%00000001 ; Clear display
   JSR lcd_send_instruction
 
   ; write letters in LCD monitor
-  LDA #"H" # ascii code of letter 'H'
+  LDA #"H" ; ascii code of letter 'H'
   JSR lcd_print_char
-  LDA #"e" # ascii code of letter 'e'
+  LDA #"e" ; ascii code of letter 'e'
   JSR lcd_print_char
-  LDA #"l" # ascii code of letter 'l'
+  LDA #"l" ; ascii code of letter 'l'
   JSR lcd_print_char
-  LDA #"l" # ascii code of letter 'l'
+  LDA #"l" ; ascii code of letter 'l'
   JSR lcd_print_char
-  LDA #"o" # ascii code of letter 'o'
+  LDA #"o" ; ascii code of letter 'o'
   JSR lcd_print_char
-  LDA #"," # ascii code of letter ','
+  LDA #"," ; ascii code of letter ','
   JSR lcd_print_char
-  LDA #" " # ascii code of letter ' '
+  LDA #" " ; ascii code of letter ' '
   JSR lcd_print_char
-  LDA #"w" # ascii code of letter 'w'
+  LDA #"w" ; ascii code of letter 'w'
   JSR lcd_print_char
-  LDA #"o" # ascii code of letter 'o'
+  LDA #"o" ; ascii code of letter 'o'
   JSR lcd_print_char
-  LDA #"r" # ascii code of letter 'r'
+  LDA #"r" ; ascii code of letter 'r'
   JSR lcd_print_char
-  LDA #"l" # ascii code of letter 'l'
+  LDA #"l" ; ascii code of letter 'l'
   JSR lcd_print_char
-  LDA #"d" # ascii code of letter 'd'
+  LDA #"d" ; ascii code of letter 'd'
   JSR lcd_print_char
-  LDA #"!" # ascii code of letter '!'
+  LDA #"!" ; ascii code of letter '!'
   JSR lcd_print_char
 
-  # work as if it is the end of the program
+  ; work as if it is the end of the program
 loop:
   JMP loop
 
   ; subrutines
 lcd_send_instruction:
-  # PHA # put current value of Accumulator in Stack if needed
+  ; PHA ; put current value of Accumulator in Stack if needed
 
-  STA PORTB # send data stored in Accumulator to port B
+  STA PORTB ; send data stored in Accumulator to port B
 
-  # sending instruction protocol
-  LDA #0 # clear dummy setting protocol, just in case
+  ; sending instruction protocol
+  LDA #0 ; clear dummy setting protocol, just in case
   STA PORTA
-  LDA #E # send instruction
+  LDA #E ; send instruction
   STA PORTA
-  LDA #0 # clear setting protocol
+  LDA #0 ; clear setting protocol
   STA PORTA
 
-  # PLA # pull value in Stack to Accumulator if needed
+  ; PLA ; pull value in Stack to Accumulator if needed
 
-  # 750 nop instructions for give delay for giving enough time to LCD montitor to execute each instruction
+  ; 750 nop instructions for give delay for giving enough time to LCD montitor to execute each instruction
   NOP
   NOP
   NOP
@@ -853,24 +853,24 @@ lcd_send_instruction:
   NOP
   NOP
 
-  RTS # return from subrutine
+  RTS ; return from subrutine
 
 lcd_print_char:
-  STA PORTB # send data stored in Accumulator to port B
+  STA PORTB ; send data stored in Accumulator to port B
 
-  # sending data protocol
-  LDA #RS # clear dummy data protocol, just in case
+  ; sending data protocol
+  LDA #RS ; clear dummy data protocol, just in case
   STA PORTA
-  LDA #E # send data
+  LDA #E ; send data
   STA PORTA
-  LDA #RS # clear data protocol
+  LDA #RS ; clear data protocol
   STA PORTA
 
-  RTS # return from subrutine
+  RTS ; return from subrutine
 
-  # from 0xFFFC we save 0x00 and 0x80 which is starting execution address
+  ; from 0xFFFC we save 0x00 and 0x80 which is starting execution address
   .org $FFFC
   .word init
 
-  # to save last 2-byte of EEPROM (any data is okay)
+  ; to save last 2-byte of EEPROM (any data is okay)
   .word $0000
